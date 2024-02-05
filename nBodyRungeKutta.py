53d51d9664a5ef6dbe878c2289505a55398f3659
Ben Lowe <79094373+BenLowe2003@users.noreply.github.com>
2024-02-05 17:17:47 +0000

    Update nBodyRungeKutta.py

--- nBodyRungeKutta.py
+++ nBodyRungeKutta.py
@@ -7,8 +7,9 @@ import random as rand
 innit_conditions = [ {"i" : 0, "position" : np.array([0.0,1.0]), "velocity" : np.array([1, 0.5]), "mass" : 1.0},
                      {"i" : 1, "position" : np.array([0.0,-1.0]), "velocity" : np.array([-1.5, 0.5]), "mass" : 1.0},
                      {"i" : 2, "position" : np.array([1.0,0.0]), "velocity" : np.array([0.5, -0.1]), "mass" : 1.0},]
-#fix
-def innit_random(n):
+
+
+def innit_random(n = 2):
     innit_conditions = [{"i" : None, "position" : np.array([None,None]), "velocity" : np.array([None, None]), "mass" : None} for i in range(n)] 
     for i in range(n):
         innit_conditions[i]["i"] = i
@@ -32,7 +33,7 @@ def gravity(state, particle, time_step, G = 1):
     return acceleration
 
 
-def simulation(iterations = 10000 , total_time = 20, innit_conditions = innit_conditions, acceleration_calculation = gravity):
+def simulation(iterations = 10000 , total_time = 5, innit_conditions = innit_random(), acceleration_calculation = gravity):
 
     time_step = total_time/iterations
     state = innit_conditions
@@ -43,17 +44,29 @@ def simulation(iterations = 10000 , total_time = 20, innit_conditions = innit_co
     for j in range(iterations):
         time += time_step
         for i in range(len(state)):
+            #Find k_1
             particle = state[i]
             k_1 = acceleration_calculation(state, particle, 0)
-            factor = np.array([time_step / 2] * dimensions)
-            particle["velocity"] = state[i]["velocity"] + k_1 * factor
+
+            # Find k_2
+            particle = state[i]
+            particle["velocity"] += time_step / 6 * k_1
+            particle["position"] += particle["velocity"] * (time_step / 2)
             k_2 = acceleration_calculation(state, particle, time_step/2)
-            particle["velocity"] = state[i]["velocity"] + k_2 * factor
-            k_3 = acceleration_calculation(state, particle, time_step/2)
-            factor = np.array([time_step] * dimensions)
-            particle["velocity"] = state[i]["velocity"] + k_3 * factor
+
+            # Find k_4
+            particle = state[i]
+            k_4_temp = acceleration_calculation(state, particle, time_step)
+            particle["velocity"] += k_4_temp * time_step
+            particle["position"] += particle["velocity"] * time_step
             k_4 = acceleration_calculation(state, particle, time_step)
 
+            #Find k_3
+            particle["velocity"] += k_4 * time_step
+            particle["position"] -= particle["velocity"] * (time_step / 2)
+            k_3 = acceleration_calculation(state, particle, -time_step/2)
+                                           
+            # Evaluate the velocity and position ofor the next time step.
             state[i]["velocity"] += time_step / 6 * sum([k_1, k_2, k_2, k_3, k_3, k_4])
             state[i]["position"] += state[i]["velocity"] * time_step
             
@@ -73,22 +86,12 @@ def print_sim(time_states, window_size = 1024):
         for i in t:
             #print("position: " + str(i["position"]) + "\n velocity " + str(i["velocity"]))
             p = gr.Point(int(i["position"][0]*100+window_size/2), int(i["position"][1]*+window_size/2))
-            c = gr.Circle(p, 1, color = [255,255,255])
-            c.draw(window)
+            c = gr.Circle(p, 1)
+            c.draw(window)
+
 
     window.getMouse()
     window.close()
-        
-
-
-
-
-
-
-
-
-
-    
-        
+     
             
             
